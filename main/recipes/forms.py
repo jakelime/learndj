9a1977f53656575f8922743bb506e4f7cadcadf5
Jake Lim <jake.lim@ams-osram.com>
2022-12-05 21:49:14 +0800

    lesson63 completed

--- main/recipes/forms.py
+++ main/recipes/forms.py
@@ -4,11 +4,34 @@ from .models import Recipe, RecipeIngredient
 
 
 class RecipeForm(forms.ModelForm):
-    name = forms.CharField()
+    error_css_class = "error-field"
+    required_css_class = "required-field"
+    name = forms.CharField(
+        label="Recipe Name",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Recipe name"}
+        ), help_text="HelpText: The name of the recipe"
+    )  # input field
+    description = forms.CharField(widget=forms.Textarea(attrs={"rows": 3}))
+    # descriptions = forms.CharField(
+    #     widget=forms.Textarea(attrs={"rows": 3})
+    # )  # we can even make new variable that did not exist in the model
 
     class Meta:
         model = Recipe
         fields = ["name", "description", "directions"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields:
+            new_data = {
+                "placeholder": f"Recipe {str(field)}",
+                "class": "form-control",
+            }
+            self.fields[str(field)].widget.attrs.update(
+                new_data
+            )
 
 
 class RecipeIngredientForm(forms.ModelForm):
--- main/recipes/views.py
+++ main/recipes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
+from django.forms.models import modelformset_factory  # model form for query set
 
 from .models import Recipe, RecipeIngredient
 from .forms import RecipeForm, RecipeIngredientForm
@@ -11,27 +12,21 @@ from .forms import RecipeForm, RecipeIngredientForm
 @login_required
 def recipe_list_view(request):
     qs = Recipe.objects.filter(user=request.user)
-    context = {
-        "objects": qs
-    }
+    context = {"objects": qs}
     return render(request, "recipes/list.html", context)
+
 
 @login_required
 def recipe_detail_view(request, id=None):
     obj = get_object_or_404(Recipe, id=id)
-    context = {
-        "object": obj
-    }
+    context = {"object": obj}
     return render(request, "recipes/detail.html", context)
-
 
 
 @login_required
 def recipe_create_view(request):
     form = RecipeForm(request.POST or None)
-    context = {
-        "form": form
-    }
+    context = {"form": form}
     if form.is_valid():
         obj = form.save(commit=False)
         obj.user = request.user
@@ -39,21 +34,33 @@ def recipe_create_view(request):
         return redirect(obj.get_absolute_url())
     return render(request, "recipes/create-update.html", context)
 
+
 @login_required
 def recipe_update_view(request, id=None):
     obj = get_object_or_404(Recipe, id=id, user=request.user)
     form = RecipeForm(request.POST or None, instance=obj)
-    form_2 = RecipeIngredientForm(request.POST or None)
+    RecipeIngredientFormset = modelformset_factory(
+        RecipeIngredient, form=RecipeIngredientForm, extra=0
+    )
+    qs = obj.recipeingredient_set.all()
+    formset = RecipeIngredientFormset(request.POST or None, queryset=qs)
     context = {
         "form": form,
-        "form_2": form_2,
+        "formset": formset,
         "object": obj,
     }
-    if all([form.is_valid(), form_2.is_valid()]):
-        form.save(commit=False)
-        form_2.save(commit=False)
+    if all([form.is_valid(), formset.is_valid()]):
+        parent = form.save(commit=False)
+        parent.save()
+        for form in formset:
+            # or, we can simply do formset.save()
+            child = form.save(commit=False)
+            if child.recipe is None:
+                print("Added new")
+                child.recipe = parent
+            child.save()
+
         print(f"{form.cleaned_data}")
-        print(f"{form_2.cleaned_data}")
-        # form.save()
-        context['message'] = 'Data saved.'
+
+        context["message"] = "Data saved."
     return render(request, "recipes/create-update.html", context)
